b4d60de9140de02544af85b27a93b1041c7389b1
Alvaro Rocha <alvjeff@hotmail.com>
2026-08-16 22:52:22 -0300

    codigo inicial do exercicio secao 4_3_8_LAB

---
+++ modulo-4/secao_4_3_8_LAB.py
@@ -0,0 +1,33 @@
+### CONVERTENDO CONSUMO DE COMBUSTÍVEL ###
+
+"""
+O consumo de combustível de um carro pode ser expresso de várias maneiras diferentes. Por exemplo, na Europa, ele é mostrado como a quantidade de combustível consumida por 100 quilômetros.
+
+Nos EUA, é mostrado como o número de quilômetros percorridos por um carro usando um litro de combustível.
+
+Sua tarefa é escrever um par de funções convertendo l/100 km em mpg e vice-versa.
+
+As funções:
+
+    - são nomeados liters_100km_to_miles_gallon e miles_gallon_to_liters_100km respectivamente;
+    - use um argumento (o valor correspondente aos nomes)
+
+Preencha o código no editor e execute-o para verificar se a sua saída é igual à nossa.
+"""
+
+def liters_100km_to_miles_gallon(liters):
+ #
+ # Escreva seu código aquie your code here.
+ #
+
+def miles_gallon_to_liters_100km(miles):
+ #
+ # Escreva seu código aqui.
+ #
+
+print(liters_100km_to_miles_gallon(3.9))
+print(liters_100km_to_miles_gallon(7.5))
+print(liters_100km_to_miles_gallon(10.))
+print(miles_gallon_to_liters_100km(60.3))
+print(miles_gallon_to_liters_100km(31.4))
+print(miles_gallon_to_liters_100km(23.5))
